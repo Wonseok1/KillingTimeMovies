7e6a9ee134ccdf96003d01af89179f7ff29cd61e
JangWonseok <jws7077@gmail.com>
2019-01-09 06:07:48 +0900

    Recycler view Okhttp

---
+++ ApiEx/app/src/main/java/com/example/jws/apiex/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.jws.apiex;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Button;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private MyRecyclerViewAdapter adapter;
+    ArrayList<Movie> movieList;
+    Button bt_movie_add;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        bt_movie_add =findViewById(R.id.bt_movie_add);
+        movieList = new ArrayList<Movie>();
+
+        //Asynctask - OKHttp
+        MyAsyncTask mAsyncTask = new MyAsyncTask();
+        mAsyncTask.execute();
+
+        //LayoutManager
+        recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this, 3));
+    }
+
+    public class MyAsyncTask extends AsyncTask<String, Void, Movie[]> {
+        //로딩중 표시
+        ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            progressDialog.setMessage("\t로딩중...");
+            //show dialog
+            progressDialog.show();
+        }
+
+        @Override
+        protected Movie[] doInBackground(String... strings) {
+            OkHttpClient client = new OkHttpClient();
+            String url1 = ApiInfo.host + ApiInfo.apikey + ApiInfo.language;
+            url1 += "&page=1" + ApiInfo.release_date_lte + ApiInfo.getTime + ApiInfo.sortdesc;
+
+            Request request = new Request.Builder()
+                    .url(url1)
+                    .build();
+            try {
+                Response response = client.newCall(request).execute();
+                Gson gson = new GsonBuilder().create();
+                JsonParser parser = new JsonParser();
+
+
+                JsonElement rootObject = parser.parse(response.body().charStream())
+                        .getAsJsonObject().get("results");
+                Movie[] posts = gson.fromJson(rootObject, Movie[].class);
+                return posts;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(Movie[] result) {
+            super.onPostExecute(result);
+            progressDialog.dismiss();
+
+
+
+
+            //ArrayList에 차례대로 집어 넣는다.
+            if (result.length > 0) {
+                for (Movie p : result) {
+                    movieList.add(p);
+                }
+            }
+
+            //어답터 설정
+            adapter = new MyRecyclerViewAdapter(MainActivity.this, movieList);
+            recyclerView.setAdapter(adapter);
+        }
+
+
+    }
+
+}
