43fbb2894f64df1ce59c73d6dfa5311518cacdb6
Jang Wonseok <jws7077@gmail.com>
2019-01-09 16:16:45 +0900

    코드 간소화
    중복된 코드 삭제(requestMovie)

--- MyMovie/app/src/main/java/com/example/jws/mymovie/MainActivity.java
+++ MyMovie/app/src/main/java/com/example/jws/mymovie/MainActivity.java
@@ -44,7 +44,8 @@ public class MainActivity extends AppCompatActivity {
 
     int a1 = 1;
     int b1 = 0;
-
+    int i1;
+    int i;
 
 
     MovieListAdapter adapter;
@@ -57,25 +58,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         movie_main_list = findViewById(R.id.movie_main_list);
         adapter = new MovieListAdapter();
         movie_main_list.setAdapter(adapter);
 
-
-
-        //DB 오픈 및 테이블 생성
-
-        /*database = openOrCreateDatabase("movie", MODE_PRIVATE, null);//DB오픈
-        if (database != null) {
-            database.execSQL("CREATE TABLE IF NOT EXISTS movieinfo (id integer PRIMARY KEY, title text)");
-            database.execSQL("CREATE TABLE IF NOT EXISTS imginfo(id integer PRIMARY KEY, img blob)");
-        }*/
-
         MovieTask task = new MovieTask();
         task.execute();
-
-
 
         //textView = findViewById(R.id.textView);
         Button button = findViewById(R.id.button);
@@ -92,10 +80,28 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 if (Movie_tab == 0) {
-                    requestMovieList();
-                } else if (Movie_tab ==1) {
-                    b1=0;
-                    requestMovieList2();
+                    //requestMovieList();
+
+                    for(i=a; b<2; i++) {
+                        b++;
+                        a++;
+                        String url = ApiInfo.host + ApiInfo.apikey + ApiInfo.language;
+                        url += "&page=" + i;
+                        url += ApiInfo.release_date_lte + ApiInfo.getTime + ApiInfo.sortdesc;
+                        requestMovieList3(url);
+                    }
+
+                } else if (Movie_tab == 1) {
+                    b1 = 0;
+                    //requestMovieList2();
+
+                    for (i1 = a1; b1 < 2; i1++) {
+                        b1++;
+                        a1++;
+                        String url = "https://api.themoviedb.org/3/movie/upcoming?api_key=e331a939fea1530cdc641ac98d848eee&language=ko-KR";
+                        url += "&page=" + i1;
+                        requestMovieList3(url);
+                    }
                 }
 
             }
@@ -110,14 +116,21 @@ public class MainActivity extends AppCompatActivity {
                 //어댑터 목록 삭제제
                 adapter = new MovieListAdapter();
                 movie_main_list.setAdapter(adapter);
-                a1=1;
-                b1=0;
+                a1 = 1;
+                b1 = 0;
 
                 /*if (ApiInfo.requestQueue == null) {//requestQueue 생성
                     ApiInfo.requestQueue = Volley.newRequestQueue(getApplicationContext());
                 }*/
 
-                requestMovieList2();
+                for (i1 = a1; b1 < 2; i1++) {
+                    b1++;
+                    a1++;
+                    String url = "https://api.themoviedb.org/3/movie/upcoming?api_key=e331a939fea1530cdc641ac98d848eee&language=ko-KR";
+                    url += "&page=" + i1;
+                    requestMovieList3(url);
+                }
+                //requestMovieList2();
             }
         });
 
@@ -128,13 +141,21 @@ public class MainActivity extends AppCompatActivity {
                 Movie_tab = 0;
                 adapter = new MovieListAdapter();
                 movie_main_list.setAdapter(adapter);
-                a=1;
-                b=0;
+                a = 1;
+                b = 0;
 
-                requestMovieList();
+                for(i=a; b<2; i++) {
+                    b++;
+                    a++;
+                    String url = ApiInfo.host + ApiInfo.apikey + ApiInfo.language;
+                    url += "&page=" + i;
+                    url += ApiInfo.release_date_lte + ApiInfo.getTime + ApiInfo.sortdesc;
+                    requestMovieList3(url);
+                }
+
+                //requestMovieList();
             }
         });
-
 
 
     }
@@ -142,7 +163,6 @@ public class MainActivity extends AppCompatActivity {
     class MovieListAdapter extends BaseAdapter {
 
         ArrayList<MovieListItem> items = new ArrayList<MovieListItem>();
-
 
         @Override
         public int getCount() {
@@ -165,7 +185,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            MovieListItemView view= null;
+            MovieListItemView view = null;
 
             if (convertView == null) {
                 view = new MovieListItemView(getApplicationContext());
@@ -173,42 +193,35 @@ public class MainActivity extends AppCompatActivity {
                 view = (MovieListItemView) convertView;
             }
 
-
-
             final MovieListItem item = items.get(position);
             view.setTitle(item.getTitle());
-           // view.setScore(item.getVote_average());
-            view.setImage(item.getBitmap(),getApplicationContext());
-
+            // view.setScore(item.getVote_average());
+            view.setImage(item.getBitmap(), getApplicationContext());
 
             movie_main_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                     Intent intent = new Intent(getApplicationContext(), MovieDetailActivity.class);
-
                     MovieListItem item2 = items.get(position);
 
                     intent.putExtra("vote_count", item2.getVote_count());
                     intent.putExtra("id", item2.getId());
                     intent.putExtra("vote_average", item2.getVote_average());
-                    intent.putExtra("title",item2.getTitle());
+                    intent.putExtra("title", item2.getTitle());
                     intent.putExtra("original_title", item2.getOriginal_title());
                     intent.putExtra("bitmap", item2.getBitmap());
                     intent.putExtra("original_language", item2.getOriginal_language());
                     intent.putExtra("overview", item2.getOverview());
                     intent.putExtra("release_date", item2.getRelease_date());
-
                     getApplicationContext().startActivity(intent);
                 }
             });
-
             return view;
         }
     }
 
     //영화 정보 가져오는 AsynkTask
     class MovieTask extends AsyncTask<Void, Void, Void> {//1 doinbackground 2 onprogressupdate 3 postexcute
-
 
         @Override
         protected Void doInBackground(Void... voids) {
@@ -217,15 +230,18 @@ public class MainActivity extends AppCompatActivity {
             if (ApiInfo.requestQueue == null) {//requestQueue 생성
                 ApiInfo.requestQueue = Volley.newRequestQueue(getApplicationContext());
             }
-            requestMovieList2();
+            //requestMovieList2();
+            for (i1 = a1; b1 < 2; i1++) {
+                b1++;
+                String url = "https://api.themoviedb.org/3/movie/upcoming?api_key=e331a939fea1530cdc641ac98d848eee&language=ko-KR";
+                url += "&page=" + i1;
+                requestMovieList3(url);
+            }
             return null;
         }
 
         @Override
         protected void onPostExecute(Void aVoid) {
-
-
-
             Toast.makeText(getApplicationContext(), "DB로딩완료", Toast.LENGTH_SHORT).show();
             super.onPostExecute(aVoid);
         }
@@ -238,10 +254,83 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void requestMovieList() {
+
+
+    public void requestMovieList3(String url) {
+        //https://api.themoviedb.org/3/movie/upcoming?api_key=e331a939fea1530cdc641ac98d848eee&language=ko-KR&page=1
+        //String url = "https://api.themoviedb.org/3/movie/upcoming?api_key=e331a939fea1530cdc641ac98d848eee&language=ko-KR&page=1";
+        StringRequest request = new StringRequest(
+                Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        processResponse(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //println("에러발생 ->" + error.getMessage());
+                    }
+                }
+        );
+        request.setShouldCache(false);
+        ApiInfo.requestQueue.add(request);
+    }
+
+    public void processResponse(String response) {
+        json = response; //받아온 전체 데이터
+        try {
+            //검색시 전체정보 파싱
+            JSONObject root = new JSONObject(json);
+            int page = root.getInt("page");
+            int total_results = root.getInt("total_results");
+            total_pages = root.getInt("total_pages");
+
+            JSONArray movie_list = root.getJSONArray("results"); //데이터중에 []안의 내용
+
+            //영화 검색결과 파싱
+            JSONArray movie_info = new JSONArray(movie_list.toString());
+
+            //println("현재페이지 : " + page);
+            //println("총 검색 영화수 : " + total_results);
+            //println("총페이지 : " + total_pages);
+            //println("현재 페이지 영화수 :"+movie_list.length());
+
+            for (int i = 0; i < total_pages; i++) {
+                //영화 상세정보 파싱
+                JSONObject movie_detail = new JSONObject(movie_info.getString(i));
+                int vote_count = movie_detail.getInt("vote_count");
+                int id = movie_detail.getInt("id");
+                double vote_average = movie_detail.getDouble("vote_average");
+                String title = movie_detail.getString("title");
+                int popularity = movie_detail.getInt("popularity");
+                String poster_path = movie_detail.getString("poster_path");
+                String original_language = movie_detail.getString("original_language");
+                String overview = movie_detail.getString("overview");
+                String release_date = movie_detail.getString("release_date");
+                String original_title = movie_detail.getString("original_title");
+
+                final String img_url = "https://image.tmdb.org/t/p/w500/" + poster_path;
+
+                adapter.addItem(new MovieListItem(vote_count, id, vote_average, title, img_url, original_language, overview, release_date, original_title));//
+                adapter.notifyDataSetChanged();
+            }
+            //api 출력 형식이 {[{ 이런 식이여서 객체안에 배열안에 객체를 다시 생성하는 식으로로
+        } catch (Exception e) {
+        }
+    }
+
+    public void println(String data) {
+        textView.append(data + "\n");
+    }
+    //textView 에 출력
+
+    /*public void requestMovieList() {
         String url = ApiInfo.host + ApiInfo.apikey + ApiInfo.language;
         url += "&page=1";
-        url +=ApiInfo.release_date_lte + ApiInfo.getTime + ApiInfo.sortdesc;//최신 개봉영화
+        url += ApiInfo.release_date_lte + ApiInfo.getTime + ApiInfo.sortdesc;//최신 개봉영화
 
         StringRequest request = new StringRequest(
                 Request.Method.GET,
@@ -251,7 +340,7 @@ public class MainActivity extends AppCompatActivity {
                     public void onResponse(String response) {
 
                         for (int i = a; b < 2; i++) {
-                            String url = ApiInfo.host + ApiInfo.apikey + ApiInfo.language ;
+                            String url = ApiInfo.host + ApiInfo.apikey + ApiInfo.language;
                             url += "&page=" + i;
                             url += ApiInfo.release_date_lte + ApiInfo.getTime + ApiInfo.sortdesc;
                             b++;
@@ -349,68 +438,12 @@ public class MainActivity extends AppCompatActivity {
         ApiInfo.requestQueue.add(request);
 
         //println("영화목록요청보냄");
-
-    }
-
-    /*public byte[] bitmapToByte(Bitmap bitmap) {
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-        byte[] byteArray = stream.toByteArray();
-        return byteArray;
     }*/
-
-    public void processResponse(String response) {
-        json = response; //받아온 전체 데이터
-        try {
-            //검색시 전체정보 파싱
-            JSONObject root = new JSONObject(json);
-            int page = root.getInt("page");
-            int total_results = root.getInt("total_results");
-            total_pages = root.getInt("total_pages");
-
-            JSONArray movie_list = root.getJSONArray("results"); //데이터중에 []안의 내용
-
-            //영화 검색결과 파싱
-            JSONArray movie_info = new JSONArray(movie_list.toString());
-
-            //println("현재페이지 : " + page);
-            //println("총 검색 영화수 : " + total_results);
-            //println("총페이지 : " + total_pages);
-            //println("현재 페이지 영화수 :"+movie_list.length());
-
-           for (int i = 0; i < total_pages; i++) {
-                //영화 상세정보 파싱
-                JSONObject movie_detail = new JSONObject(movie_info.getString(i));
-                int vote_count = movie_detail.getInt("vote_count");
-                int id = movie_detail.getInt("id");
-                double vote_average = movie_detail.getDouble("vote_average");
-                String title = movie_detail.getString("title");
-                int popularity = movie_detail.getInt("popularity");
-                String poster_path = movie_detail.getString("poster_path");
-                String original_language = movie_detail.getString("original_language");
-                String overview = movie_detail.getString("overview");
-                String release_date = movie_detail.getString("release_date");
-               String original_title = movie_detail.getString("original_title");
-
-                final String img_url = "https://image.tmdb.org/t/p/w500/"+poster_path;
-
-
-                adapter.addItem(new MovieListItem(vote_count, id, vote_average, title, img_url,original_language, overview, release_date, original_title ));//
-                adapter.notifyDataSetChanged();
-            }
-            //api 출력 형식이 {[{ 이런 식이여서 객체안에 배열안에 객체를 다시 생성하는 식으로로
-        } catch (Exception e) {
-        }
-    }
-
-    public void println(String data) {
-        textView.append(data + "\n");
-    }
-    //textView 에 출력
-
 
 
 }
+
+
 
 
 
